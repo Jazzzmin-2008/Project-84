b90ab1db94e7fb84e38e7c0225a2aa30ecf3bb88
Jazzzmin-2008 <82570532+Jazzzmin-2008@users.noreply.github.com>
2021-08-17 15:32:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,57 +1,80 @@
-canvas = document.getElementById("myCanvas");
-ctx = canvas.getContext("2d");
+var canvas = document.getElementById("myCanvas");
+ctx = canvas.getcontext ("2d");
 
-background_img = "Bg-1";
-key_img = "main.png";
+img_width = 300;
+img_height = 100;
 
-function add(){
-    background_imgtag = new Image()
-    background_imgtag.onload = uploadbackground;
-    background_imgtag.src = background_img;
+var img_image;
 
-    key_imgtag = new Image()
-    key_imgtag.onload = uploadkey;
-    key_imgtag.src = key_img;
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
 }
 
-window.addEventListener("keydown",my_keydown);
+function uploadimg() {
 
- function my_keydown(e){
-    keyPressed = e.keycode
-    console.log(keyPressed)
-
-    if((keyPressed >=97 && keyPressed <=122)(keyPressed >=65 && keyPressed <=90)){
-        alphabet();
-        document.getElementById("d1").innerHTML="You pressed an alphabet key";
-        console.log("alphabet");
-    }
-    if(keyPressed >=48 && keyPressed <=57){
-        number();
-        document.getElementById("d1").innerHTML="You pressed a number key";
-        console.log("number");
-    }
-    if(keyPressed >=37 && keyPressed <=40){
-        arrow();
-        document.getElementById("d1").innerHTML="You pressed an arrow key";
-        console.log("arrow");
-    }
-    if(keyPressed = 17/18/27){
-        special();
-        document.getElementById("d1").innerHTML="You pressed an special key(alt,ctrl,esc)";
-        console.log("arrow");
-    }
-} 
-
-
-function alphabet(){
-    key_img="Alphabet.png";
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
-function number(){
-    key_img="Number.png";
+
+//Write a code to grab the key-pressed event
+window.addEventListener("keydown" , my_keydown());
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+
+	    if((keyPressed >=45 && keyPressed<=57)){
+		numberkey();
+		document.getElementById("d1").innerHTML = "You pressed a number key";
+		console.log("number key");
+	}	
+	    if((keyPressed >=37 && keyPressed<=40)){
+		arrowkey();
+		document.getElementById("d1").innerHTML = "You pressed an arrow key";
+		console.log("arrow key");
+    }		
+	    if(keyPressed=17 && keyPressed=18 && keyPressed=27){
+		specialkey();
+		document.getElementById("d1").innerHTML = "You pressed a special key";
+		console.log("special key")
+	}	
+	    if ((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)) {
+			aplhabetkey();
+		document.getElementById("d1").innerHTML = "You pressed an alphabet key";
+		console.log("alphabet key")
+		} else {
+			otherkey();
+		document.getElementById("d1").innerHTML = "You pressed symbol or other key";
+		}
 }
-function special(){
-    key_img="Special.png";
+
+function aplhabetkey()
+{
+	img_image = "Alphabet.png";
+	add();
+
 }
-function arrow(){
-    key_img="Arrow.png";
+function numberkey()
+{
+	img_image = "Number.png";
+	add();
+}
+function arrowkey()
+{
+	img_image = "Arrow.png";
+	add();
+}
+function specialkey()
+{
+	img_image = "Special.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
 }
